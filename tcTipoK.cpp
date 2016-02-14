fcf880d3714782cff67036ecbc3f5bfc03c175b5
itismattei <robotics.itismattei@gmail.com>
2016-02-14 13:57:24 +0100

    Prima release.

---
+++ tcTipoK.cpp
@@ -0,0 +1,22 @@
+
+
+#include "Energia.h"
+
+// include the SPI library:
+#include <SPI.h>
+
+void setup();
+void loop();
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly: 
+  
+}
+
+
